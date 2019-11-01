9dc15ddd474a67e4c778f95e059492cb6ffc9fe1
爱悠的沫夏 <oncwnuMRLtgs-1vuQSn30RxDTltI@git.weixin.qq.com>
2019-11-01 14:44:15 +0800

    为方便阅读 为Models添加Display特性

--- Models/Base_Book .cs
+++ Models/Base_Book .cs
@@ -1,12 +1,20 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace HandBookApi.Models
-{
+{ /// <summary>
+    /// 号码簿实体
+    /// </summary>
     public class Base_Book
     {
+          
+        [Display(Name = "号码簿ID")]
         public long Id { get; set; }
+         [Display(Name = "号码簿名称")]
         public string Name { get; set; }
+          [Display(Name = "备注")]
         public string ReMark { get; set; }
+            [Display(Name = "创建时间")]
         public DateTime CreateDate { get; set; }
     }
 }
--- Models/Game_Setting.cs
+++ Models/Game_Setting.cs
@@ -1,14 +1,25 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace HandBookApi.Models
 {
+     /// <summary>
+    /// 游戏试玩实体
+    /// </summary>
     public class Game_Setting
     {
+         
+        [Display(Name = "游戏试玩ID")]
         public long Id { get; set; }
+        [Display(Name = "游戏试玩Url")]
         public string Url{ get; set; }
+        [Display(Name = "用户名")]
         public string UserName { get; set; }
+        [Display(Name = "密码")]
          public string PassWord { get; set; }
+           [Display(Name = "备注")]
         public string ReMark { get; set; }
+         [Display(Name = "创建时间")]
         public DateTime CreateDate { get; set; }
     }
 }
