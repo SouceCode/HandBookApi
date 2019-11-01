787e38b990bc72dc2c96136ca7611b4840bba707
爱悠的沫夏 <oncwnuMRLtgs-1vuQSn30RxDTltI@git.weixin.qq.com>
2019-11-01 10:02:32 +0800

    提高性能 把默认模板使用的 使用连接池方式连接数据库

--- Startup.cs
+++ Startup.cs
@@ -26,9 +26,12 @@ namespace HandBookApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddControllers();
-             services.AddDbContext<HandBookContext>(options =>
-            options.UseSqlite(Configuration.GetConnectionString("HandBookContext1")));
+            //  services.AddDbContext<HandBookContext>(options =>
+            // options.UseSqlite(Configuration.GetConnectionString("HandBookContext1")));
+            services.AddDbContextPool<HandBookContext>(builder=> builder.UseSqlite(Configuration.GetConnectionString("HandBookContext1")));
+
             #region  中间件
             // Register the Swagger services
               services.AddSwaggerDocument();
